4af73f29bda578c143f8d6a222741689eda26f5a
Vit Benton <Vit@vitbenton.com>
2025-05-29 15:16:58 -0400

    type -> interface for extend feature

--- src/types/repository.ts
+++ src/types/repository.ts
@@ -1,23 +1,19 @@
-export type BasicRepositoryType = {
+export interface BasicRepositoryType {
   created_at: string
   id: string
   name: string
   owner: string
 }
 
-export type RepositoryType = {
+export interface RepositoryType extends BasicRepositoryType {
   allow_forking: boolean
   archived: boolean
-  created_at: string
   description: string
   has_downloads: boolean
   homepage: string
   html_url: string
-  id: string
   isPrivate: boolean
   language: string
-  name: string
-  owner: string
   owner_url: string
   size: number
   stargazers_count: number
