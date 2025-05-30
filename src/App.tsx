cbc1e618b9162a7c0452816e2947f242b7804d54
Vit Benton <Vit@vitbenton.com>
2025-05-30 10:37:38 -0400

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+import '@/App.css'
 import { Route, Routes } from 'react-router-dom'
 import { SearchProvider } from '@/context/SearchContext'
 import NotFoundRedirect from '@/components/routing/NotFoundRedirect'
