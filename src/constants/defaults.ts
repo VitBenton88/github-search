96db0f3f6b5df79b45a0fbd2650975335639c080
Vit Benton <Vit@vitbenton.com>
2025-06-04 12:15:48 -0400

    default repository const created

---
+++ src/constants/defaults.ts
@@ -0,0 +1,20 @@
+import type { RepositoryType } from '@/types/repository'
+
+export const defaultRepository: RepositoryType = {
+  description: '',
+  id: '',
+  name: '',
+  owner: '',
+  allow_forking: false,
+  archived: false,
+  created_at: '',
+  has_downloads: false,
+  homepage: '',
+  html_url: '',
+  isPrivate: false,
+  language: '',
+  owner_url: '',
+  size: 0,
+  stargazers_count: 0,
+  updated_at: '',
+}
