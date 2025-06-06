9ae47a3d9da8ef97791b011ec54575d81b64b74c
Vit Benton <Vit@vitbenton.com>
2025-06-06 12:32:29 -0400

    configure eslint-plugin-import

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import importPlugin from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -14,27 +15,46 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      'import': importPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'type'
+          ],
+          'newlines-between': 'never',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
+        }
+      ],
       'no-multi-spaces': ['error'],
       'no-trailing-spaces': 'error',
       'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-        'jsx-quotes': ['error', 'prefer-double'],
+      'jsx-quotes': ['error', 'prefer-double'],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'semi': ['error', 'never'],
       "sort-imports": [
-        "error", 
-        { 
-          "ignoreCase": true, 
-          "ignoreDeclarationSort": true 
+        "error",
+        {
+          "ignoreCase": true,
+          "ignoreDeclarationSort": true
         }
-      ], 
+      ],
     },
   },
 )
