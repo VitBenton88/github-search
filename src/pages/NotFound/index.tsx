12b92f7c767f0677a1fd3bd50d92b2c50faafeb8
Vit Benton <Vit@vitbenton.com>
2025-06-09 18:00:26 -0400

    improved 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,11 +1,14 @@
 import { Nav } from '@/components'
+import './NotFound.css'
 
 const NotFound: React.FC = () => {
 
   return (
     <>
       <Nav data-testid="nav" />
-      <h1 data-testid="heading">Page not found!</h1>
+      <main id="not-found">
+        <h1 data-testid="heading">Page not found!</h1>
+      </main>
     </>
   )
 }
