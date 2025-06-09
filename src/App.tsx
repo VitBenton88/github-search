122a5d0845cea38366abb9bbee83d587a07ddfe8
Vit Benton <Vit@vitbenton.com>
2025-06-09 15:58:30 -0400

    barrel file for pages dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import '@/App.css'
 import { Route, Routes } from 'react-router-dom'
 import { NotificationProvider } from '@/context/notification/NotificationProvider'
 import { SearchProvider } from '@/context/search/SearchProvider'
-import NotFound from '@/pages/NotFound'
-import Repository from '@/pages/Repository'
-import Search from '@/pages/Search'
+import { NotFound, Repository, Search } from '@/pages'
 
 function App() {
   return (
---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as NotFound } from './NotFound'
+export { default as Repository } from './Repository'
+export { default as Search } from './Search'
