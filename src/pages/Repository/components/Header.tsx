418825f4925d3352662f96ca7a3b483896f5436b
Vit Benton <Vit@vitbenton.com>
2025-06-17 15:49:52 -0400

    lint fix

--- src/pages/Repository/components/Header.tsx
+++ src/pages/Repository/components/Header.tsx
@@ -25,9 +25,9 @@ const Header: React.FC<HeaderProps> = ({ ...props }) => {
     <header {...props}>
       <h1 data-testid="title">{name}</h1>
       <small aria-label={`${stargazers_count} stars`} data-testid="star-count">⭐ {stargazers_count}</small>
-      {description?.trim() &&
-        (<h2 data-testid="description">{description}</h2>)
-      }
+      {description?.trim() && (
+        <h2 data-testid="description">{description}</h2>
+      )}
       <h3>Owner: <Link href={owner_url} data-testid="owner-link">{owner} &rarr;</Link></h3>
       <h4 data-testid="created">Created: {formatDisplayDate(created_at)}</h4>
       <h4 data-testid="updated">Updated: {formatDisplayDate(updated_at)}</h4>
