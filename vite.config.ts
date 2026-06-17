28472f925ba2e7ac1d1563ed0100671204c00662
Vit Benton <Vit@vitbenton.com>
2026-06-17 11:36:08 -0400

    gh pages install

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const viteConfig = defineViteConfig({
+  base: '/github-search/',
   plugins: [react()],
   resolve: {
     alias: {
